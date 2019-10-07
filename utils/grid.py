9df8228383f036bb286675cc0634b715894428cb
Max Langenkamp <maxnz@mit.edu>
2019-10-07 12:02:19 -0400

    undid some bad merges

--- utils/grid.py
+++ utils/grid.py
@@ -1,11 +1,6 @@
 import random
 import numpy as np
-<<<<<<< HEAD
-=======
-from utils import Train, OtherMask
-import random
-from graphics import display_grid
->>>>>>> c44724af5493d58fa04201fdf13eb902d468cf0b
+
 
 
 """
@@ -35,14 +30,14 @@ class Grid:
         actions = set([(0, 0), (-1, 0), (0, 1), (1, 0), (0, -1)])
 
 =======
-                        
+
 
     def __init__(self, size, num_agents=1):
         assert isinstance(size, int)
 
         # available actions: stay, north, east, south, west
         self.all_actions = set([(0, 0), (-1, 0), (0, 1), (1, 0), (0, -1)])
-        
+
 >>>>>>> c44724af5493d58fa04201fdf13eb902d468cf0b
         self.size = size
 
@@ -62,11 +57,8 @@ class Grid:
         pass
 
     def _place_agent(self) -> None:
-<<<<<<< HEAD
         empty = self.grid_coords - {self.train.pos} - set(self.other_agents)
-=======
-        empty = self.grid_coords - {self.train.pos} - self.other_agents.get_mask_set()
->>>>>>> c44724af5493d58fa04201fdf13eb902d468cf0b
+
         self.agent_pos = random.choice(tuple(empty))
 
     def in_bounds(self,position:tuple) -> bool:
@@ -85,44 +77,26 @@ class Grid:
             new_position_x = self.agent_pos[1]+action[1]
             if not self.in_bounds((new_position_y,new_position_x)):
                 legal_actions -= {action}
-    
+
         return legal_actions
-<<<<<<< HEAD
 
 
-=======
-        
->>>>>>> c44724af5493d58fa04201fdf13eb902d468cf0b
-
-        
     def T(self, action:tuple) -> None:
         """
         Precondition: action needs to be legal
         Returns new state, internally updates
         """
-<<<<<<< HEAD
-        #check that action is legal
-        if action not in self.legal_actions():
-            raise "Not a valid action"
-
-        ag = self.agent_pos
-        ac = action
-        self.train.update
-        new_agent_pos = (ag[0] + ac[0], ag[1] + ac[1])
-
-=======
 
         #check that action is legal
         if action not in self.legal_actions():
             print(self.legal_actions())
             raise ValueError("Not a valid action")
-        
+
         ag = self.agent_pos
         ac = action
         self.train.update()
-        new_agent_pos = (ag[0] + ac[0], ag[1] + ac[1]) 
-        
->>>>>>> c44724af5493d58fa04201fdf13eb902d468cf0b
+        new_agent_pos = (ag[0] + ac[0], ag[1] + ac[1])
+
         #collision detect
         if new_agent_pos == self.train.pos:
             #death, terminal state?
@@ -136,38 +110,23 @@ class Grid:
                 self.other_agents.push(new_agent_pos,action)
             else:
                 new_agent_pos = self.agent_pos
-                
+
         if self.other_agents.get_mask_set().intersection({self.train.pos}):
             #death
             pass
-<<<<<<< HEAD
 
-
-
-=======
-            
         self.agent_pos = new_agent_pos
 
-    
-
-    
-        
->>>>>>> c44724af5493d58fa04201fdf13eb902d468cf0b
-        #return state+action #avoid aliasing
 
     def R(self, state: tuple, action: tuple) -> int:
         """
         """
-<<<<<<< HEAD
-
-        #reward per state
-=======
         reward = -1
         if self.agent_pos == self.train.pos:
             #death, end state
             reward = -100
         #reward per state
-    
+
 
 if __name__ == "__main__":
     grid = Grid(5)
@@ -176,4 +135,3 @@ if __name__ == "__main__":
         print("")
         action = random.choice(tuple(grid.legal_actions()))
         grid.T(action)
->>>>>>> c44724af5493d58fa04201fdf13eb902d468cf0b
