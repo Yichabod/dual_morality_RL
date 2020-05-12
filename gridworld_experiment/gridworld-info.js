e1ae790fa55b3f388447f92e0662cf5ce3329f04
Max Langenkamp <maxnz@mit.edu>
2020-05-12 15:56:15 -0700

    added functions for switch and train demos

--- gridworld_experiment/gridworld-info.js
+++ gridworld_experiment/gridworld-info.js
@@ -34,3 +34,73 @@ function pushDemo(GridWorldTask){
     });
     task.start();
 }
+function trainDemo(GridWorldTask){
+    document.getElementById('next45').disabled = true
+    let task = new GridWorldTask({
+        reset: true,
+        container: $("#taskinfo5")[0],
+        reward_container: $("#rewardinfo5")[0],
+        step_callback: (d) => {console.log(d)},
+        endtask_callback: (result_data,r) => {
+            pushDemo(GridWorldTask);
+            if (r == 3){
+                document.getElementById("next45").disabled = false
+            }//button name needs change
+        }
+    });
+    task.init({
+        init_state: {
+            'agent': [2,2],
+            'cargo1': [4,1],
+            'cargo2': [1,4],
+            'train': [1,1],
+            'trainvel': [1,0]
+        },
+        switch_pos: [7,7],
+        targets: {
+            'target1': [6,6],
+            'target2': [7,6]
+        },
+        show_rewards: true,
+        wait_time: 0,
+        time_limit: undefined,
+        best_reward: -1,
+    });
+    task.start();
+}
+function switchDemo(GridWorldTask){
+    document.getElementById('next45').disabled = true
+    let task = new GridWorldTask({
+        reset: true,
+        container: $("#taskinfo6")[0],
+        reward_container: $("#rewardinfo6")[0],
+        step_callback: (d) => {console.log(d)},
+        endtask_callback: (result_data,r) => {
+            pushDemo(GridWorldTask);
+            if (r == 3){
+                document.getElementById("next45").disabled = false
+            }//button name needs change
+        }
+    });
+    task.init({
+        init_state: {
+            'agent': [2,3],
+            'cargo1': [4,2],
+            'cargo2': [1,4],
+            'train': [1,2],
+            'trainvel': [1,0]
+        },
+        switch_pos: [4,3],
+        targets: {
+            'target1': [6,6],
+            'target2': [7,6]
+        },
+        show_rewards: true,
+        wait_time: 0,
+        time_limit: undefined,
+        best_reward: -1,
+    });
+    task.start();
+}
+
+
