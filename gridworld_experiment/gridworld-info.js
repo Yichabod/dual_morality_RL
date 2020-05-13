a226a86de7f2561f4b042dd608fc1f5587075c0f
Max Langenkamp <maxnz@mit.edu>
2020-05-12 20:28:31 -0700

    got the demos for train and switch to work

--- gridworld_experiment/gridworld-info.js
+++ gridworld_experiment/gridworld-info.js
@@ -1,4 +1,6 @@
 info4_done = false;
+info5_done = false;
+info6_done = false;
 
 function clearGrid(div){
     myNode = document.getElementById(div);
@@ -48,19 +50,25 @@ function pushDemo(GridWorldTask){
     task.start();
 }
 function trainDemo(GridWorldTask){
-    document.getElementById('next45').disabled = true
-    let task = new GridWorldTask({
+    if (!info5_done){
+    document.getElementById('next56').style.color = "red";
+    document.getElementById('next56').disabled = true;
+    }
+        let task = new GridWorldTask({
         reset: true,
         container: $("#taskinfo5")[0],
         reward_container: $("#rewardinfo5")[0],
-        step_callback: (d) => {console.log(d)},
+        step_callback: (d) => {
+            if (d['reward'] == -1 && d['iter']==5){
+                document.getElementById("next56").disabled = false
+                info5_done = true;
+                document.getElementById('next56').style.color = "white"
+            }
+        },
         endtask_callback: (result_data,r) => {
-            pushDemo(GridWorldTask);
-            if (r == 3){
-                document.getElementById("next45").disabled = false
-            }//button name needs change
+            trainDemo(GridWorldTask);
         }
-    });
+            });
     task.init({
         init_state: {
             'agent': [2,2],
@@ -71,8 +79,8 @@ function trainDemo(GridWorldTask){
         },
         switch_pos: [7,7],
         targets: {
-            'target1': [6,6],
-            'target2': [7,6]
+            'target1': [2,3],
+            'target2': [1,0]
         },
         show_rewards: true,
         wait_time: 0,
@@ -82,36 +90,42 @@ function trainDemo(GridWorldTask){
     task.start();
 }
 function switchDemo(GridWorldTask){
-    document.getElementById('next45').disabled = true
-    let task = new GridWorldTask({
+    if (!info6_done){
+    document.getElementById('next67').style.color = "red";
+    document.getElementById('next67').disabled = true;
+    }
+        let task = new GridWorldTask({
         reset: true,
         container: $("#taskinfo6")[0],
         reward_container: $("#rewardinfo6")[0],
-        step_callback: (d) => {console.log(d)},
+        step_callback: (d) => {
+            if (d['reward'] == 0 && d['iter']==5){
+                document.getElementById("next67").disabled = false
+                info6_done = true;
+                document.getElementById('next67').style.color = "white"
+            }
+        },
         endtask_callback: (result_data,r) => {
-            pushDemo(GridWorldTask);
-            if (r == 3){
-                document.getElementById("next45").disabled = false
-            }//button name needs change
+            switchDemo(GridWorldTask);
         }
-    });
-    task.init({
+            });
+        task.init({
         init_state: {
             'agent': [2,3],
             'cargo1': [4,2],
-            'cargo2': [1,4],
+            'cargo2': [6,6],
             'train': [1,2],
             'trainvel': [1,0]
         },
         switch_pos: [4,3],
         targets: {
-            'target1': [6,6],
-            'target2': [7,6]
+            'target1': [2,3],
+            'target2': [2,4]
         },
         show_rewards: true,
         wait_time: 0,
         time_limit: undefined,
-        best_reward: -1,
+        best_reward: 0,
     });
     task.start();
 }
