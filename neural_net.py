063c1ed836eab739c50c40dcfd96c6af9257ac2d
Max Langenkamp <maxnz@mit.edu>
2020-06-19 11:33:13 -0400

    fixed the neural net predict function, changed neural net params, added a trained neural net fom colab

--- neural_net.py
+++ neural_net.py
@@ -10,15 +10,25 @@ cuda = True if torch.cuda.is_available() else False
 NUM_TARGETS = 2
 CHANNELS = 9
 
+NN_FILE = 'nn_model'
+
 class Net(nn.Module):
-    def __init__(self, C=CHANNELS):
+    def __init__(self, C=CHANNELS, dropout_p=0.2):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(C, 100, 3, padding=1)
+        self.conv1 = nn.Conv2d(C, 100, 3, padding=1) # mixing channels a bad thing? Look at other deep gridworld architectures
         self.conv2 = nn.Conv2d(100, 100, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv3 = nn.Conv2d(100, 100, 3, padding=1)
         self.conv4 = nn.Conv2d(100, 100, 3, padding=1)
-        self.fc1 = nn.Linear(100 * 1 * 1, 5)
+        # self.conv3_1 = nn.Conv2d(100, 100, 3, padding=1)
+
+        # self.conv3_2 = nn.Conv2d(100, 100, 3, padding=1)
+        # self.conv3_3 = nn.Conv2d(100, 100, 3, padding=1)
+
+        # add fully connected layer here
+        self.fc1 = nn.Linear(100 * 1 * 1, 64)
+        self.fc2 = nn.Linear(64, 5)
+        # self.dropout = nn.Dropout(dropout_p)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -26,8 +36,37 @@ class Net(nn.Module):
         x = F.relu(self.conv3(x))
         x = self.pool(F.relu(self.conv4(x)))
         x = x.view(-1, 100 * 1 * 1)
-        x = self.fc1(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
+# class Net(nn.Module):
+#     def __init__(self, C=CHANNELS, dropout_p=0.2):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(C, 100, 3, padding=1)
+#         self.conv2 = nn.Conv2d(100, 100, 3, padding=1)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv3 = nn.Conv2d(100, 100, 3, padding=1)
+#         self.conv3_1 = nn.Conv2d(100, 100, 3, padding=1)
+#
+#         self.conv3_2 = nn.Conv2d(100, 100, 3, padding=1)
+#         self.conv3_3 = nn.Conv2d(100, 100, 3, padding=1)
+#         self.conv4 = nn.Conv2d(100, 100, 3, padding=1)
+#         self.fc1 = nn.Linear(100 * 1 * 1, 5)
+#         # self.dropout = nn.Dropout(dropout_p)
+#
+#     def forward(self, x):
+#         x = F.relu(self.conv1(x))
+#         x = self.pool(F.relu(self.conv2(x)))
+#         x = F.relu(self.conv3(x))
+#         x = F.relu(self.conv3_1(x))
+#         x = self.pool(F.relu(self.conv4(x)))
+#         x = F.relu(self.conv3_2(x))
+#         # x = self.dropout(x)
+#         x = F.relu(self.conv3_3(x))
+#         x = x.view(-1, 100 * 1 * 1)
+#         x = self.fc1(x)
+#         return x
+
 
 def make_onehot_data(inputs, labels):
     """
@@ -37,47 +76,22 @@ def make_onehot_data(inputs, labels):
     inputs = torch.from_numpy(inputs).float()#.to(torch.long)
     labels = torch.from_numpy(labels).float()
 
+    # NEWER ATTEMPT TO CLEAN UP INPUT
+
     base = inputs[:, 0:1, :, :] #this contains all elts except next train/targets
     targets = inputs[:, 2:3, :, :]
 
     mask_layer = (base != 0).float()
     agent_layer = (base == 1).float()
-    train_layer = (base == 3).float()
+    train_layer = (base == 2).float()
     next_train_layer = inputs[:, 1:2, :, :]
-    switch_layer = (base == 4).float()
-    object1_layer = (base == 5).float()
+    switch_layer = (base == 3).float()
+    object1_layer = (base == 4).float()
     target1_layer = (targets == 1).float()
-    object2_layer = (base == 6).float()
+    object2_layer = (base == 5).float()
     target2_layer = (targets == 2).float()
 
-    onehot_inputs = torch.cat((mask_layer,agent_layer,train_layer, next_train_layer,switch_layer,object1_layer,target1_layer,object2_layer, target2_layer), dim=1)
-
-
-
-    # #only keep current trains
-    # inputs = inputs[:, 0:1, :, :]
-    #
-    # B, H, W = inputs.shape[0], inputs.shape[2], inputs.shape[3]
-    # # input shape b x c x h x w to net
-    # # so need to unsqueeze to make channel dimension of 1, could also make each agent its own channel instead
-    # #torch.from_numpy(inputs).unsqueeze(1).to(torch.long)
-    # # C - 3 (for targets and next train) + 1 since zero indexed? TODO
-    # onehot_inputs = torch.zeros([B, CHANNELS-2, H, W], dtype=torch.float32)
-    #
-    # #put agent, others, switch, train into their own layer
-    # onehot_inputs.scatter_(1, inputs, torch.ones(onehot_inputs.shape))
-    #
-    # targets = torch.from_numpy(targets).to(torch.long)
-    # onehot_targets = torch.zeros([B, NUM_TARGETS+1, H, W], dtype=torch.float32) #+1 for zero index
-    # onehot_targets.scatter_(1, targets, torch.ones(onehot_targets.shape))
-    # onehot_targets = onehot_targets[:,1:,:,:] #get rid of zero mask
-    #
-    # #add previous train observations
-    # onehot_inputs = torch.cat((onehot_inputs, torch.from_numpy(next_trains).float()), dim=1)
-    # #add targets
-    # onehot_inputs = torch.cat((onehot_inputs, onehot_targets), dim=1)
-    # import pdb; pdb.set_trace()
-    # onehot_inputs = torch.cat((onehot_inputs[:,:2,:,:], onehot_inputs[:,3:,:,:]), dim=1)
+    onehot_inputs = torch.cat((mask_layer, agent_layer, train_layer, next_train_layer,switch_layer,object1_layer,target1_layer,object2_layer, target2_layer), dim=1)
 
     if cuda:
         onehot_inputs = onehot_inputs.cuda()
@@ -92,6 +106,8 @@ def make_onehot_data(inputs, labels):
 
     return onehot_train_inputs, onehot_test_inputs, train_labels, test_labels
 
+
+
 def train(grids_file="grids_data.npy",actions_file="actions_data.npy",num_epochs=20, C=CHANNELS):
     '''
     C is the number of channels in input array
@@ -101,14 +117,13 @@ def train(grids_file="grids_data.npy",actions_file="actions_data.npy",num_epochs
 
     xs = np.load(grids_file)
     ys = np.load(actions_file)
-    np.random.shuffle(xs)
 
     onehot_train_xs, onehot_test_xs, train_ys, test_ys = make_onehot_data(xs, ys)
 
 
     net = Net(C)
-    criterion = nn.CrossEntropyLoss() #nn.MSELoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    criterion = nn.MSELoss()#CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.0001) #original lr 0.001
 
     if cuda:
         net = net.cuda()
@@ -120,7 +135,8 @@ def train(grids_file="grids_data.npy",actions_file="actions_data.npy",num_epochs
         #trains on the remainder if not gully divisible
         for j in range((len(train_ys)-1)//batch_size+1):
             inputs, labels = onehot_train_xs[batch_size*j:batch_size*(j+1)], train_ys[batch_size*j:batch_size*(j+1)]
-            labels = torch.argmax(labels, dim=1).long()
+
+            # labels = torch.argmax(labels, dim=1).long()
             # zero the parameter gradients
             optimizer.zero_grad()
 
@@ -143,7 +159,7 @@ def train(grids_file="grids_data.npy",actions_file="actions_data.npy",num_epochs
                     outputs = net(inputs)
                     label_argmax = torch.argmax(labels, dim=1)
                     output_argmax = torch.argmax(outputs, dim=1)
-
+                    # labels = label_argmax.long()
                     accuracy = torch.mean((label_argmax==output_argmax).float())
                     test_accuracy.append(accuracy.item())
 
@@ -155,36 +171,57 @@ def train(grids_file="grids_data.npy",actions_file="actions_data.npy",num_epochs
     print("training took", time.time() - start, "seconds")
     torch.save(net.state_dict(), 'nn_model')
     print("Model saved as nn_model")
-    
+
 def load(C=CHANNELS):
     model = Net(C)
     if not cuda:
-        model.load_state_dict(torch.load('nn_model',map_location='cpu'))
+        model.load_state_dict(torch.load(NN_FILE ,map_location='cpu'))
     else:
-        model.load_state_dict(torch.load('nn_model'))
+        model.load_state_dict(torch.load(NN_FILE))
     return model
 
 def predict(model, state, C=CHANNELS):
     '''
     model: pytorch model, output of load()
-    input_state: 2xHxW(5x5) numpy array
+    state: 3x5x5 numpy array corresponding to grid
     returns 1x5(num actions) numpy array of Q-values for each action
     '''
-    _, H, W = state.shape
-    onehot_test_xs = torch.zeros([1, C-1, H, W])
+    # _, H, W = state.shape
+    # onehot_test_xs = torch.zeros([1, C-1, H, W])
+    #
+    # #state[1] is current observation
+    # test_x = torch.from_numpy(state[0]).unsqueeze(0).unsqueeze(1).to(torch.long)
+    # onehot_test_xs.scatter_(1, test_x, torch.ones(onehot_test_xs.shape))
+    #
+    # next_trains = state[1:2]
+    # onehot_test_xs = torch.cat((onehot_test_xs, torch.from_numpy(next_trains).unsqueeze(0).float()), dim=1)
 
-    #state[1] is current observation
-    test_x = torch.from_numpy(state[0]).unsqueeze(0).unsqueeze(1).to(torch.long)
-    onehot_test_xs.scatter_(1, test_x, torch.ones(onehot_test_xs.shape))
+    inputs = torch.from_numpy(state).float()#.to(torch.long)
 
-    next_trains = state[1:2]
-    onehot_test_xs = torch.cat((onehot_test_xs, torch.from_numpy(next_trains).unsqueeze(0).float()), dim=1)
-    outputs = model(onehot_test_xs)
+    # NEWER ATTEMPT TO CLEAN UP INPUT
+
+    base = inputs[0:1, :, :] #this contains all elts except next train/targets
+    targets = inputs[2:3, :, :]
+
+    mask_layer = (base != 0).float()
+    agent_layer = (base == 1).float()
+    train_layer = (base == 2).float()
+    next_train_layer = inputs[1:2, :, :]
+    switch_layer = (base == 3).float()
+    object1_layer = (base == 4).float()
+    target1_layer = (targets == 1).float()
+    object2_layer = (base == 5).float()
+    target2_layer = (targets == 2).float()
+
+    onehot_inputs = torch.cat((mask_layer, agent_layer, train_layer, next_train_layer,switch_layer,object1_layer,target1_layer,object2_layer, target2_layer), dim=0)
+    onehot_inputs = torch.unsqueeze(onehot_inputs, dim=0)
+    outputs = model(onehot_inputs)
 
     return outputs.detach().numpy()
+
 
 if __name__ == "__main__":
     #grids = np.ones((49,2,5,5))
     #actions = np.ones((49,5))
 
-    train(grids_file='shuffled_grids_may28.npy',actions_file='shuffled_actions_may28.npy', num_epochs=50)
+    train(grids_file='small_grids.npy',actions_file='small_actions.npy', num_epochs=50)
