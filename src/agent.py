6c1b25983fba8a3bb2245c531772e861c3d617a7
Max Langenkamp <maxnz@mit.edu>
2020-07-30 17:46:30 -1000

    made folders and did some anslysis

---
+++ src/agent.py
@@ -0,0 +1,259 @@
+import numpy as np
+from collections import Counter, defaultdict
+import neural_net
+from graphics import display_grid
+from utils import generate_array, in_bounds
+import new_grid as grid
+#import torch
+
+ACTION_DICT = {(0, 0):0, (-1, 0):1, (0, 1):2, (1, 0):3, (0, -1):4} #
+
+class Agent:
+    """
+    Dual processing agent.
+    Given a set of actions and a view of the grid, decides what action to take
+    """
+
+    def __init__(self):
+        pass
+
+    def train_load_neural_net(self):
+        """
+        Attempt to load neural net from 'neural_net' file. If not present, train neural net
+        and then return the network
+        """
+        try:
+            return self.net
+        except(AttributeError):
+            try:
+                net = neural_net.load()
+                print("neural net loaded")
+            except:
+                print("training neural net")
+                neural_net.train()
+                net = neural_net.load()
+            self.net = net
+            return net
+
+    def neural_net_output(self, grid):
+        net = self.train_load_neural_net()
+        state_array = generate_array(grid)
+        out = neural_net.predict(net, state_array)
+        return out[0]
+
+    def run_model_free_policy(self, grid, display=False):
+        """
+        Use neural network to solve MDP (no exploration)
+        args:
+        grid = grid object
+        display = whether to display the grid
+        return_grid_type = whether t
+        returns:
+        np array of grids where agent has taken action according to policy
+        np array of actions that agent took
+        type of grid (agent hit by train, agent push switch, agent push others, train hit others): str
+        """
+        if display: display_grid(grid)
+        net = self.train_load_neural_net()
+
+        total_reward = 0
+
+        while not grid.terminal_state: # max number of steps per episode
+            # grids.append(state)
+            test_input = generate_array(grid)
+
+            #input to neural net predict should be (3,5,5) for base, next_train, targets
+            action_ind = np.argmax(neural_net.predict(net, test_input))
+            action = grid.all_actions[action_ind]
+            if display: print(neural_net.predict(net, test_input))
+            if display: print(action)
+
+            total_reward += grid.R(action)
+            grid.T(action)
+            if display: display_grid(grid)
+        return total_reward
+
+    def _create_epsilon_greedy_policy(self, Q_dict, epsilon=0.2, nn_init = False):
+        """
+        Use Q_dict to create a greedy policy
+        args: Q_dict[state] = [value of action1, value of action2, ...]
+                where state is defined as result of grid.current_state (self.agent_pos,self.train.pos,list(self.other_agents.positions)[0])
+        returns: policy function takes in state and chooses with prob 1-e+(e/|A|) maxQ value action
+        """
+        def policy(grid):
+            state = grid.current_state
+
+            if state not in Q_dict and nn_init:
+                Q_dict[state] = self.neural_net_output(grid)
+
+            Q_values = Q_dict[state]
+
+            action_probs = [0 for k in range(len(Q_values))]
+            best_action = np.argmax(Q_values)
+            for i in range(len(Q_values)):
+                if np.count_nonzero(Q_values) == 0: #all are zero
+                    action_probs[i] = 1/len(Q_values)
+                elif i == best_action:
+                    action_probs[i] = 1-epsilon+(epsilon/len(Q_values))
+                else:
+                    action_probs[i] = epsilon/len(Q_values)
+            return action_probs
+        return policy
+
+    def run_final_policy(self, grid, Q_dict, nn_init=False, display=False):
+        """
+        Use Q_dict to solve MDP (no exploration)
+        args: grid = original grid state, Q_dict[state] = [value of action1, value of action2, ...]
+        nn_init = whether we want to use the neural network to initialise the monte carlo policy
+        returns: 2 numpy arrays containing grid and optimal action pairs to be
+        fed into downstream model
+        """
+        policy = self._create_epsilon_greedy_policy(Q_dict,epsilon=0,nn_init=nn_init) #optimal policy, eps=0 always chooses best value
+        #if display: display_grid(grid)
+        state = grid.current_state
+        total_reward = 0 #to keep track of most significant action taken by agent
+        # grids_array = np.empty((1,grid.size,grid.size),dtype=int)
+        grids_array = []
+        action_val_array = np.empty((1,grid.size),dtype=int)
+
+        while not grid.terminal_state: # max number of steps per episode
+            action_probs = policy(grid)
+            action_ind = np.argmax(action_probs)
+            if display:
+                pass#print(Q_dict[state])
+            action = grid.all_actions[action_ind]
+            if display: display_grid(grid)
+            if display: print(action)
+            action_val_array = np.concatenate((action_val_array,np.array([Q_dict[grid.current_state]])))
+            grids_array.append(generate_array(grid))
+            total_reward += grid.R(action)
+            newstate = grid.T(action)
+            state = newstate
+        grids_array = np.array(grids_array)
+        if display: print(total_reward)
+        return grids_array, action_val_array[1:], total_reward
+
+
+    def mc_first_visit_control(self, start_grid, iters, discount_factor=0.9, epsilon=0.2, nn_init=False, cutoff = 0) -> tuple:
+        """
+        Monte Carlo first visit control. Uses epsilon greedy strategy
+        to find optimal policy. Details can be found page 101 of Sutton
+        Barto RL Book
+        Args: nn_init whether to initialize Q-values with neural net outputs
+        Returns: (Q_values, policy)
+                Q(s,a) = val, policy(state) = action
+        """
+        # Q is a dictionary mapping state to [value of action1, value of action2,...]
+        grid = start_grid.copy()
+
+        if nn_init:
+            Q = {}
+        else:
+            Q = defaultdict(lambda: list(0 for i in range(len(grid.all_actions))))
+
+        policy = self._create_epsilon_greedy_policy(Q,epsilon, nn_init)
+
+        sa_reward_sum, total_sa_counts = defaultdict(int), defaultdict(int) #keep track of total reward and count over all episodes
+        for n in range(iters):
+            # generate episode
+            episode = []
+            grid = start_grid.copy() #copy because running episode mutates grid object
+            state = grid.current_state
+            while not grid.terminal_state: # max number of steps per episode
+                action_probs = policy(grid)
+
+                action_ind = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+                action = grid.all_actions[action_ind]
+
+                #must calculate reward before transitioning state, otherwise reward will be calculated for action in newstate
+                reward = grid.R(action)
+                newstate = grid.T(action)
+                episode.append((state, action, reward))
+                state = newstate
+                #display_grid(grid)
+            sa_counts = Counter([(x[0],x[1]) for x in episode]) #dictionary: [s,a]=count
+            G = 0 #averaged reward
+            for t in range(len(episode)-1,-1,-1):
+                G = discount_factor*G + episode[t][2] #reward at the next time step
+                state = episode[t][0]
+                action = episode[t][1]
+                action_index = grid.all_actions.index(action)
+                sa_pair = state, action
+                sa_counts[sa_pair] -= 1
+                if sa_counts[sa_pair] == 0: #appears for the first time
+                    sa_reward_sum[sa_pair] += G
+                    total_sa_counts[sa_pair] += 1
+                    Q[state][action_index] = sa_reward_sum[sa_pair]/total_sa_counts[sa_pair] #average reward over all episodes
+                    policy = self._create_epsilon_greedy_policy(Q, epsilon,nn_init)
+
+        return Q, policy
+
+
+if __name__ == "__main__":
+
+    #push_init_pos = {'train':(2,0),'agent':(4,1),'other1':(3,2),'switch':(0,0),'other2':(2,4),'other1num':1,'other2num':4}
+    #switch_init_pos = {'train':(2,0),'agent':(4,1),'other1':(0,0),'switch':(3,2),'other2':(2,4),'other1num':1,'other2num':4}
+
+    #here agent only needs to push cargo into target, which is near train. To test model free
+    easy1 = {'train':(1,0),'trainvel':(0,1),'cargo1':(3,2),'num1':1,'target1':(2,2),
+            'switch':(0,0),'agent':(4,2),'cargo2':(2,4),'num2':2,'target2':(0,3)}
+    # agent should push simple cargo into target, away from train
+    easy2 = {'train':(1,0),'trainvel':(0,1),'cargo1':(4,1),'num1':1,'target1':(4,0),
+            'switch':(0,0),'agent':(2,2),'cargo2':(2,4),'num2':2,'target2':(0,3)}
+    # agent should get out of the way of the train
+    easy3 = {'train':(1,0),'trainvel':(0,1),'cargo1':(4,1),'num1':1,'target1':(4,0),
+            'switch':(0,0),'agent':(1,2),'cargo2':(2,4),'num2':2,'target2':(0,3)}
+    # dont_die_pos = {'train':(2,0),'trainvel':()
+    easy4 = {'train':(2,0),'trainvel':(0,1),'cargo1':(3,2),'num1':1,'target1':(4,0),
+            'switch':(0,0),'agent':(2,2),'cargo2':(2,4),'num2':2,'target2':(0,3)}
+    #somewhere between 10,000 and 50,000 iterations the mc finally gets it - seems pretty hard without nn even
+    push3 = {'train':(1,0),'trainvel':(0,1),'cargo1':(2,3),'num1':1,'target1':(3,1),
+            'switch':(4,0),'agent':(3,3),'cargo2':(2,4),'num2':2,'target2':(1,4)}
+
+    #this one takes a long time too - perhaps because reward comes too late
+    death1 = {'train':(0,0),'trainvel':(0,1),'cargo1':(1,2),'num1':1,'target1':(2,2),
+            'switch':(4,0),'agent':(0,3),'cargo2':(2,4),'num2':2,'target2':(3,3)}
+
+    push1 = {'train':(1,0),'trainvel':(0,1),'cargo1':(2,2),'num1':1,'target1':(3,1),
+        'switch':(0,0),'agent':(3,3),'cargo2':(1,4),'num2':2,'target2':(0,3)}
+
+    targets_test = {'train':(0,0),'trainvel':(0,1),'cargo1':(1,2),'num1':1,'target1':(1,3),
+            'switch':(4,4),'agent':(2,1),'cargo2':(2,2),'num2':2,'target2':(3,2)}
+
+    weird1 = {'train':(4,2),'trainvel':(-1,0),'other1':(4,3),'num1':1,'target1':(0,3),
+            'switch':(1,0),'agent':(3,0),'other2':(2,2),'num2':2,'target2':(2,4)}
+
+    switch = {'train':(1,0),'trainvel':(0,1),'cargo1':(2,1),'num1':1,'target1':(4,3),
+            'switch':(3,3),'agent':(4,4),'cargo2':(1,2),'num2':2,'target2':(0,3)}
+
+    push4 = {'train':(2,0),'trainvel':(0,1),'cargo1':(3,2),'num1':1,'target1':(0,1),
+        'switch':(4,0),'agent':(4,2),'cargo2':(3,3),'num2':2,'target2':(0,3)}
+
+    test3 = {"train": (0, 3), "trainvel": (1, 0), "cargo1": (2, 2), "target1": (0, 4), "switch": (2, 4), "agent": (2, 0), "cargo2": (3, 3), "target2": (3, 4),'num1':1,
+    'num2':2}
+
+    test12 = {"train": (1, 0), "trainvel": (0, 1), "cargo1": (2, 2), "target1": (3, 1), "switch": (0, 4), "agent": (3, 1), "cargo2": (1, 4), "target2": (0, 3), 'num1':1, 'num2':2}
+
+    test14 = {"train": (4, 4), "trainvel": (-1, 0), "cargo1": (1, 3), "target1": (3, 2), "switch": (4, 1), "agent": (0, 2), "cargo2": (0, 4), "target2": (0, 1), 'num1':1,
+    'num2':2}
+
+    test_suite = [grid.Grid(5,random=False, init_pos=test3),
+                  grid.Grid(5,random=False, init_pos=test12),
+                  grid.Grid(5,random=False, init_pos=test14)]
+
+    swit27 = {"train": (1, 0), "trainvel": (0, 1), "cargo1": (0, 1), "target1": (4, 3), "switch": (3, 3), "agent": (4, 4), "cargo2": (1, 2), "target2": (0, 3), 'num1':1, "num2":2}
+
+    testgrid = grid.Grid(5,random=False, init_pos=swit27)
+    agent = Agent()
+
+
+    model = 'based'
+    if model == 'dual':
+        Q, policy = agent.mc_first_visit_control(testgrid.copy(), 1, nn_init=True,cutoff=0.4)
+        agent.run_final_policy(testgrid.copy(), Q,nn_init=True,display=True)
+    if model == 'free':
+        agent.run_model_free_policy(testgrid.copy(), display=True)
+    if model == 'based':
+        Q, policy = agent.mc_first_visit_control(testgrid.copy(), iters=100, nn_init=False)
+        #display_grid(testgrid.copy())
+        agent.run_final_policy(testgrid.copy(), Q,nn_init=False,display=True)
---
+++ src/utils.py
@@ -0,0 +1,159 @@
+import numpy as np
+
+ELEMENT_INT_DICT = {'agent':1,'train':2,'switch':3,'other1':4,'other2':5}
+INT_ELEMENT_DICT = {1:'◉',2:'T',3:'T',4:'S'}
+
+def in_bounds(size,position:tuple) -> bool:
+    """
+    given positon (y,x), checks both dimensions are within the board
+    helper function called by Train and Grid classses
+    """
+    if 0 <= position[0] < size and 0 <= position[1] < size:
+        return True
+    else:
+        return False
+
+def generate_array(mdp,action=None):
+
+    """
+    Takes in a Grid mdp environment and an agent, with optional action to
+    show which direction next move should be in
+    Generates numpy array with main agent(1), other agents(depends on value), train(3),
+    switch(4), targets(depends on value of other agent)
+    in the grid of given dimensions.
+    Intended to be able to feed into a network
+    """
+
+    #  first layer for agent, train, switch, objs. Second for next_train, last for targets
+    dims = (3,mdp.size,mdp.size)
+    grid = np.full(dims, 0, dtype=int) #np has nice display built in
+    others_dict = mdp.other_agents.mask
+    #bug: need to properly distinguish between the targets
+    for other_coord, other_obj in others_dict.items():
+        #the value of the other in the grid will be the num of
+        # non other elements + value of other
+        target_coord = other_obj.target
+        if other_obj.num == 1:
+            grid[0,other_coord[0],other_coord[1]] = ELEMENT_INT_DICT['other1']
+            grid[2, target_coord[0], target_coord[1]] = 1
+        elif other_obj.num == 2:
+            grid[0,other_coord[0],other_coord[1]] = ELEMENT_INT_DICT['other2']
+            grid[2, target_coord[0], target_coord[1]] = 2
+
+    grid[0,mdp.agent_pos[0],mdp.agent_pos[1]] = ELEMENT_INT_DICT['agent']
+
+    grid[0,mdp.switch.pos[0], mdp.switch.pos[1]] = ELEMENT_INT_DICT['switch']
+    next_train_y = mdp.train.pos[0]+mdp.train.velocity[0]
+    next_train_x = mdp.train.pos[1]+mdp.train.velocity[1]
+
+    if mdp.train.on_screen == True:
+        grid[0,mdp.train.pos[0],mdp.train.pos[1]] = ELEMENT_INT_DICT['train']
+
+    if in_bounds(5,(next_train_y,next_train_x)):
+
+        grid[1, next_train_y, next_train_x] = 1
+
+    return grid
+
+def visualize_array(grid):
+    """
+    grid: 2d numpy array with integers for each element of the grid
+    prints a grid with all the elements visualised
+    """
+    ret = np.full(grid.shape, "_", dtype=str) #np has nice display built in
+    for i, row in enumerate(grid):
+        for j, elt in enumerate(row):
+            if elt in INT_ELEMENT_DICT:
+                ret[i][j] = INT_ELEMENT_DICT[elt]
+    print(ret)
+
+
+class OtherMask:
+    """
+    Represents other agents in Grid MDP including their position and number
+    """
+
+    def __init__(self, positions=[(1,3)], num=[1], init={}, targets=[(1,4)]):
+        """
+        """
+        self.mask = {}
+        self.init = init
+        self.targets = []
+        self.positions = []
+
+        if len(init) > 0:
+            self.mask = init
+            self.positions = list(self.mask.keys())
+            for pos in self.mask:
+                self.targets.append(self.mask[pos].get_target())
+        else:
+            self.positions = positions
+            for idx,pos in enumerate(positions):
+                self.mask[pos] = Other(num[idx],targets[idx],targets[idx]==pos)
+            self.targets = targets
+
+    def push(self, position, action):
+        other_pushed = self.mask.pop(position,None)
+        new_pos = (position[0] + action[0], position[1] + action[1])
+        self.mask[new_pos] = other_pushed
+        old_pos_index = self.positions.index(position)
+        self.positions[old_pos_index] = new_pos
+
+    def copy(self):
+        new_init = {}
+        for pos in self.mask:
+            new_init[pos] = self.mask[pos].copy()
+        othermask = OtherMask(init=new_init)
+        return othermask
+
+    def get_mask(self):
+        return self.mask
+
+class Other:
+    def __init__(self,num,target,active):
+        self.target = target
+        self.num = num
+        self.active = active
+    def copy(self):
+        return Other(self.num,self.target,self.active)
+    def get_num(self):
+        return self.num
+    def get_target(self):
+        return self.target
+    def toggle_active(self):
+        self.active = not self.active
+
+class Switch:
+    """
+    Represents a switch to change the track of the train within the Grid MDP
+    """
+    def __init__(self, size, pos=(0,4)):
+        self.size = size
+        self.pos = pos
+        self.activated = False
+    def copy(self):
+        return Switch(self.size, self.pos)
+
+class Train:
+    """
+    Represents a train within the Grid MDP and its properties
+    """
+    def __init__(self, size, pos=(1,0),velocity=(0,1)):
+        self.pos = pos
+        self.velocity = velocity
+        self.size = size
+        self.on_screen = True
+    def update(self):
+        newx = self.pos[0]+self.velocity[0]
+        newy = self.pos[1]+self.velocity[1]
+        self.pos = (newx,newy)
+        if not in_bounds(self.size,self.pos):
+            self.on_screen = False
+        else:
+            self.on_screen = True
+    def get_next_position(self,velocity):
+        newx = self.pos[0]+velocity[0]
+        newy = self.pos[1]+velocity[1]
+        return (newx,newy)
+    def copy(self):
+        return Train(self.size, self.pos,self.velocity)
